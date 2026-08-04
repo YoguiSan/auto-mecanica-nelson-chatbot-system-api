8c7467f21fb5564e293adea5f954f41625a0e2e7
Nelson Salles de Barros Filho <nelsonsbf@protonmail.com>
2026-08-04 11:50:00 -0300

    build(PM2 Config): Config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     name: 'amn-chatbot-system-api',
     script: './src/index.ts',
     interpreter: 'tsx',
-    interpreter_args: '--transpile-only',  // Skip type checking
+    // interpreter_args: '--transpile-only',  // Skip type checking
     max_memory_restart: '510M',
     env: {
       NODE_ENV: 'production',
