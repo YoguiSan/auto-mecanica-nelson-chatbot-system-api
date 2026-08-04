d7027929562ba3d4209ce086a7ada4ab56579cde
Nelson Salles de Barros Filho <nelsonsbf@protonmail.com>
2026-08-04 12:20:13 -0300

    build(PM2 Config): :hammer: Script de build

--- prod-ecosystem.config.cjs
+++ prod-ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'amn-chatbot-system-api',
-    script: './dist/index.js',
+    script: './dist/index.cjs',
     max_memory_restart: '510M',
     env: {
       NODE_ENV: 'production',
